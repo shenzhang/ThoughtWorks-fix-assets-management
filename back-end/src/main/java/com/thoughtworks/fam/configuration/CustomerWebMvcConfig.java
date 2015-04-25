d33170772873090b83dfe3af271bf31443f925ed
shenzhang <shenzhang722@gamil.com>
2015-04-25 13:46:50 +0800

    Change file name

--- back-end/src/main/java/com/thoughtworks/fam/configuration/CustomerWebMvcConfig.java
+++ back-end/src/main/java/com/thoughtworks/fam/configuration/CustomerWebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.fam.configuration;
 
 import com.google.common.base.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
@@ -15,10 +17,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @Configuration
 @AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
 public class CustomerWebMvcConfig extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomerWebMvcConfig.class);
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String externalResource = System.getProperty("externalResource");
         if (!Strings.isNullOrEmpty(externalResource)) {
+            LOG.info("Install external resource: {}", externalResource);
             registry.addResourceHandler("/**").addResourceLocations(externalResource);
         }
         super.addResourceHandlers(registry);
