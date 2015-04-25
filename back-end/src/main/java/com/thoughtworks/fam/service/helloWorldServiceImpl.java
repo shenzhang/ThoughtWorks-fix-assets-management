0f4923f77a8291b6200cff13dd0a1ed78dc92c50
shenzhang <shenzhang722@gamil.com>
2015-04-25 12:25:56 +0800

    add service/dao/configuration

---
+++ back-end/src/main/java/com/thoughtworks/fam/service/helloWorldServiceImpl.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.fam.service;
+
+import com.thoughtworks.fam.dao.HelloWorldDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * User: Zhang Shen
+ * Date: 4/25/15
+ * Time: 10:28 AM
+ */
+@Service
+public class HelloWorldServiceImpl implements HelloWorldService {
+    @Autowired
+    private HelloWorldDao helloWorldDao;
+
+    @Override
+    public String getMessage() {
+        return helloWorldDao.getMessage();
+    }
+}
--- back-end/src/main/java/com/thoughtworks/fam/web/HelloWorldController.java
+++ back-end/src/main/java/com/thoughtworks/fam/web/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.fam.web;
 
+import com.thoughtworks.fam.service.HelloWorldService;
 import com.thoughtworks.fam.web.dto.HelloWorldDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/helloworld")
 public class HelloWorldController {
+    @Autowired
+    private HelloWorldService helloWorldService;
 
     @RequestMapping(method = RequestMethod.GET)
     public HelloWorldDTO say() {
         HelloWorldDTO dto = new HelloWorldDTO();
-        dto.setMessage("Hello World");
+        dto.setMessage(helloWorldService.getMessage());
         return dto;
     }
 }
