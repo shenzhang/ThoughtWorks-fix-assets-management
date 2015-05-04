62ed1e32c34dfd68002f8117ad75b7705b5c389f
Jing Lv <jinglv@thoughtworks.com>
2015-05-04 22:09:28 +0800

    Jimmy - add function for asset item button.

--- front-end/src/pages/AssetItem.react.js
+++ front-end/src/pages/AssetItem.react.js
@@ -5,16 +5,30 @@ import {
   }from 'material-ui'
 
 var AssetItem = React.createClass({
+
+  getInitialState(){
+    return {
+      msg: "你点了我!",
+      clickCount: 1
+    }
+  },
+  handleClick(){
+    this.setState({
+        clickCount: this.state.clickCount + 1
+    });
+    alert("你点了我" + this.state.clickCount + "次!")
+  },
   render() {
     return(
       <tr className="asset__item">
-        <td className="asset__attribute">{this.props.asset.name}</td>
+        <td className="asset__attribute" value="sb">{this.props.asset.name}</td>
         <td className="asset__attribute">{this.props.asset.date}</td>
         <td className="asset__attribute">{this.props.asset.number}</td>
         <td className="asset__attribute">{this.props.asset.type}</td>
         <td className="asset__attribute">
           <RaisedButton label="Button"
-                        secondary={true}>
+                        secondary={true}
+                        onClick={this.handleClick}>
           </RaisedButton>
         </td>
       </tr>
