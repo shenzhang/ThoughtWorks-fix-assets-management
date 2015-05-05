03e05f93ec9c45933257b049abf6626167696649
tuber <btu@thoughtworks.com>
2015-05-05 13:46:27 +0800

    [tuber] update login page

--- front-end/src/pages/Login.react.js
+++ front-end/src/pages/Login.react.js
@@ -4,19 +4,38 @@ import {
     Paper
     } from 'material-ui';
 import State from 'react-router';
+import userApi from './../services/user'
 
 var Login = React.createClass({
     mixins: [State],
+
+    getInitialState() {
+        return {
+            disabled: false
+        };
+    },
 
     render() {
         return (
             <Paper zDepth={1}>
                 <p>Log in</p>
+                <div>
+                    <p>User name</p>
+                </div>
                 <div></div>
-                <div></div>
-                <RaisedButton label='Log in' primary='true'></RaisedButton>
+                <RaisedButton label='Log in' primary={true} onClick={this._login} disabled={this.state.disabled}></RaisedButton>
             </Paper>
         );
+    },
+    _login() {
+        userApi.login(this.state.user, this.state.password)
+            .then(this.onLogin, this.onLoginFail)
+    },
+    onLogin(msg) {
+        alert('success');
+    },
+    onLoginFail(err) {
+        alert('failure');
     }
 });
 
