e4e31564f2b7b650ceda192a71c1e661e8c65f68
shenzhang <shenzhang722@gamil.com>
2015-05-14 01:16:53 +1000

    modify artifacts dir

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 chmod 400 id_rsa
-scp -i id_rsa backend-*.jar tw@52.68.95.171:applications/fam/application-latest.jar
+scp -i id_rsa artifacts/libs/backend-*.jar tw@52.68.95.171:applications/fam/application-latest.jar
