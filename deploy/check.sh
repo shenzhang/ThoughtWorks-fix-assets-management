ea623aa669f4fe1651b6497d3e2b4ef326dbf8a4
shenzhang <shenzhang722@gamil.com>
2015-05-14 20:49:44 +1000

    add deploy check script

---
+++ deploy/check.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+echo "Begin checking url: $URL"
+
+curl -s -m 5 -o /dev/null $URL
+
+if [ $? -ne 0 ]; then
+  echo "failed, continue..."
+else
+  echo "success"
+  exit 0
+fi
+
+echo "Check failed"
