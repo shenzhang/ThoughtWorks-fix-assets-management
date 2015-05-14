06aa4b158cd159ff6cdcbbc7a84daa13403de905
shenzhang <shenzhang722@gamil.com>
2015-05-14 20:27:37 +1000

    modify deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,4 @@
 chmod 400 id_rsa
-scp -i id_rsa artifacts/libs/backend-*.jar tw@52.68.95.171:applications/fam/application-latest.jar
-ssh -i id_rsa tw@52.68.95.171 "nohup applications/fam/deploy.sh > /dev/null 2>&1"
+
+scp -i id_rsa artifacts/libs/backend-*.jar $USER@$HOST:applications/fam/application-latest.jar
+ssh -i id_rsa $USER@$HOST "nohup applications/fam/deploy.sh > /dev/null 2>&1"
