f9fb66de3ca1b9e8c7c85a8412f7dce1a774a2a3
Jing Lv <jinglv@thoughtworks.com>
2015-05-14 18:09:43 +0800

    [#Story 1&2] - Jimmy: add test for Assets component.

---
+++ front-end/src/pages/__test__/Assets.spec.js
@@ -0,0 +1,16 @@
+require('chai').should()
+
+import React from 'react/addons'
+import Assets from '../Assets.react.js'
+const TestUtils = React.addons.TestUtils
+
+let assets
+describe('Assets Page Component', function () {
+  beforeEach(function () {
+    assets = TestUtils.renderIntoDocument(<Assets />)
+  });
+  it('should be able to initialized independently', function () {
+    TestUtils.isDOMComponent(Assets).should.be.equal(true)
+    done()
+  })
+}
