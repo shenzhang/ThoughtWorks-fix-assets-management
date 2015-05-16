26585fb38b61f0b4003078b234410cce1bf74c9d
Jing Lv <jinglv@thoughtworks.com>
2015-05-16 16:03:46 +0800

    [#Story 1&2] - Jimmy&Wen: refactor asset header and each asset row.

--- front-end/src/pages/AssetsTable.react.js
+++ front-end/src/pages/AssetsTable.react.js
@@ -7,8 +7,7 @@ import {
 
 import AssetButton from './button/AssetButton.react.js'
 
-module.exports = React.createClass({
-
+var AssetsHeader = React.createClass({
   _create_owner_name_title(){
     if (this.props.hasOwner) {
       return (
@@ -16,44 +15,66 @@ module.exports = React.createClass({
       )
     }
   },
-  _create_owner_name(asset){
+  render() {
+    return (
+      <thead>
+      <tr className="asset__head">
+        {this._create_owner_name_title()}
+        <th className="asset__head__attribute">AssetName</th>
+        <th className="asset__head__attribute">Number</th>
+        <th className="asset__head__attribute">Assigned Date</th>
+        <th className="asset__head__attribute">Type</th>
+      </tr>
+      </thead>
+    )
+  }
+})
+
+var AssetsRow = React.createClass({
+  _create_owner_name(ownerName){
     if (this.props.hasOwner) {
       return (
-        <td className="asset__attribute">{asset.ownerName}</td>
+        <td className="asset__attribute">{ownerName}</td>
       )
     }
   },
   handleClick(){
     window.location.href = "about:blank"
   },
+  render() {
+    var asset = this.props.asset
+    return (
+      <tr className="asset__item">
+        {this._create_owner_name(asset.ownerName)}
+        <td className="asset__attribute">{asset.assetName}</td>
+        <td className="asset__attribute">
+          <a className="asset__number"
+             onClick={this.handleClick}
+             title="about:blank">{asset.number}
+          </a>
+        </td>
+        <td className="asset__attribute">{asset.assignDate}</td>
+        <td className="asset__attribute">{asset.type}</td>
+        <td className="asset__action"><AssetButton className="asset__button" asset={asset}/></td>
+      </tr>
+    )
+  }
+})
+
+var AssetsTable = React.createClass({
   render(){
-    var self = this;
+    var AssetsRows = this.props.assets.map((asset) => {
+      return <AssetsRow asset={asset} hasOwner={this.props.hasOwner}/>
+    })
     return (
       <table className="my_assets_tab__container">
-        <thead>
-        <tr className="asset__head">
-          {this._create_owner_name_title()}
-          <th className="asset__head__attribute">AssetName</th>
-          <th className="asset__head__attribute">Number</th>
-          <th className="asset__head__attribute">Assigned Date</th>
-          <th className="asset__head__attribute">Type</th>
-        </tr>
-        </thead>
+        <AssetsHeader hasOwner={this.props.hasOwner}/>
         <tbody>
-        {this.props.assets.map(function (asset, index) {
-          return (
-            <tr className="asset__item" key={index}>
-              {self._create_owner_name(asset)}
-              <td className="asset__attribute">{asset.assetName}</td>
-              <td className="asset__attribute"><a className="asset__number" onClick={self.handleClick} title="about:blank">{asset.number}</a></td>
-              <td className="asset__attribute">{asset.assignDate}</td>
-              <td className="asset__attribute">{asset.type}</td>
-              <td className="asset__action"><AssetButton className="asset__button" asset={asset}/></td>
-            </tr>
-          )
-        })}
+        {AssetsRows}
         </tbody>
       </table>
     )
   }
 })
+
+export default AssetsTable
