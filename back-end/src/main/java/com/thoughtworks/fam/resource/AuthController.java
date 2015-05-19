5dd6c421d4bdcbb7bdd6a71380866f3662d03f10
tuber <btu@thoughtworks.com>
2015-05-19 08:55:36 +0800

    [tuber] change authControllerTest

--- back-end/src/main/java/com/thoughtworks/fam/resource/AuthController.java
+++ back-end/src/main/java/com/thoughtworks/fam/resource/AuthController.java
@@ -5,7 +5,6 @@ import com.thoughtworks.fam.domain.LoginInformation;
 import com.thoughtworks.fam.domain.User;
 import com.thoughtworks.fam.exception.AuthException;
 import com.thoughtworks.fam.service.AuthService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,32 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
-@RequestMapping(value = "/login")
+@RequestMapping(value = "/auth")
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
 
-    @RequestMapping(value = "/assets", method = POST)
-    public LoginInformation login(@RequestBody JSONObject jsonObject) {
-        String user = jsonObject.getString("userName");
-        String password = jsonObject.getString("password");
-        LoginInformation loginInformation = new LoginInformation();
-
-        try {
-            authService.validate(user, password);
-            loginInformation.setErrorCode(HttpStatus.OK);
-            loginInformation.setErrorMessage("Success!");
-        } catch (AuthException e) {
-            loginInformation.setErrorCode(HttpStatus.UNAUTHORIZED);
-            loginInformation.setErrorMessage(e.getMessage());
-        }
-
-        return loginInformation;
-    }
-
-    @RequestMapping(method = POST)
+    @RequestMapping(value = "/login", method = POST)
     public LoginInformation login(@RequestBody User user) {
 
         LoginInformation loginInformation = new LoginInformation();
--- back-end/src/test/java/com/thoughtworks/fam/resource/AuthControllerTest.java
+++ back-end/src/test/java/com/thoughtworks/fam/resource/AuthControllerTest.java
@@ -1,9 +1,9 @@
 package com.thoughtworks.fam.resource;
 
-import com.thoughtworks.fam.exception.AuthException;
 import com.thoughtworks.fam.domain.LoginInformation;
+import com.thoughtworks.fam.domain.User;
+import com.thoughtworks.fam.exception.AuthException;
 import com.thoughtworks.fam.service.AuthService;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -13,8 +13,6 @@ import org.springframework.http.HttpStatus;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class AuthControllerTest {
@@ -40,15 +38,11 @@ public class AuthControllerTest {
 
     @Test
     public void should_be_able_login_when_given_right_user_and_password() throws AuthException {
-        String user = "user";
-        String password = "passwords";
+        User user = new User("name", "password");
 
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("userName", user);
-        jsonObject.put("password", password);
-
-        LoginInformation responseEntity = authController.login(jsonObject);
-        verify(authService).validate(user, password);
+        LoginInformation responseEntity = authController.login(user);
+        given(authService.validate(user.getName(), user.getPassword()))
+                .willReturn(true);
         assertThat(responseEntity.getErrorCode(), is(HttpStatus.OK));
         assertThat(responseEntity.getErrorMessage(), is("Success!"));
     }
@@ -65,15 +59,13 @@ public class AuthControllerTest {
     }
 
 
-    private void expectErrorMessageForUserAndPassword(String errorMsg, String user, String password) throws AuthException {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("userName", user);
-        jsonObject.put("password", password);
+    private void expectErrorMessageForUserAndPassword(String errorMsg, String name, String password) throws AuthException {
+        User user = new User(name, password);
 
-        given(authService.validate(eq(user),eq(password)))
+        given(authService.validate(user.getName(),user.getPassword()))
                 .willThrow(new AuthException(errorMsg));
 
-        LoginInformation responseEntity = authController.login(jsonObject);
+        LoginInformation responseEntity = authController.login(user);
         assertThat(responseEntity.getErrorCode(), is(HttpStatus.UNAUTHORIZED));
         assertThat(responseEntity.getErrorMessage(), is(errorMsg));
     }
