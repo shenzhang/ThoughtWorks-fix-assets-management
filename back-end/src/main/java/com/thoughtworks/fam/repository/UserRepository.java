d1adf4f5bc6b8c8c15db38983c74bc12aeba7750
tuber <btu@thoughtworks.com>
2015-06-10 19:53:09 +0800

    [tuber] change to use JpaRepository

--- back-end/src/main/java/com/thoughtworks/fam/repository/UserRepository.java
+++ back-end/src/main/java/com/thoughtworks/fam/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.thoughtworks.fam.repository;
 
 import com.thoughtworks.fam.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByName(String name);
 }
